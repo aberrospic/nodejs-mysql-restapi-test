d95b159e782aac65a3cb774684b63e8e1cfcc82c
Abraham B <aberrospic@gmail.com>
2023-08-23 14:36:15 -0500

    env variables

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import express from "express";
-import employeesRoutes from "./routes/employees.routes.js";
-import indexRoutes from "./routes/index.routes.js";
+import { app } from "./app.js";
+import { PORT } from "./config.js";
 
-const app = express();
-
-app.use(express.json());
-
-app.use("/api", employeesRoutes);
-app.use(indexRoutes);
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Hola desde railway.app unu");
 });
